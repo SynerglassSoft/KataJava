f64c384b9c110a75fdaab84a1bf7d47571fc2d59
SynerRaphael <139222387+SynerRaphael@users.noreply.github.com>
2024-02-29 10:56:26 +0100

    Add HundredDoor

--- src/main/java/hundredDoor/HundredDoor.java
+++ src/main/java/hundredDoor/HundredDoor.java
@@ -6,14 +6,6 @@ public class HundredDoor {
     }
 
     public static void openDoor(boolean[] doors, int passes) {
-        for (int i = 0; i < passes; i++) {
-            openDoorByStep(doors, i+1);
-        }
-    }
 
-    private static void openDoorByStep(boolean[] doors, int step){
-        for (int i = step-1; i < doors.length; i+=step) {
-            doors[i] = !doors[i];
-        }
     }
 }
--- src/main/java/romanNumerals/RomanToArabicNumberConvertor.java
+++ src/main/java/romanNumerals/RomanToArabicNumberConvertor.java
@@ -2,31 +2,6 @@ package romanNumerals;
 
 public class RomanToArabicNumberConvertor {
     public static int convertRomanNumberToArabicNumber(String romanNumber) {
-        char[] allCharacter = romanNumber.toCharArray();
-        int arabicNumber = 0;
-        int convertedChar;
-        for (int i = 0; i < allCharacter.length; i++) {
-            convertedChar = convertRomanCharacterToArabicDigit(allCharacter[i]);
-            if (i+1 < allCharacter.length && convertedChar < convertRomanCharacterToArabicDigit(allCharacter[i+1])){
-                arabicNumber += convertRomanCharacterToArabicDigit(allCharacter[i+1])-convertedChar;
-                i++;
-            }else{
-                arabicNumber += convertedChar;
-            }
-        }
-        return arabicNumber;
-    }
-
-    private static int convertRomanCharacterToArabicDigit(char romanCharacter){
-        switch (romanCharacter){
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-        }
         return 0;
     }
 }
